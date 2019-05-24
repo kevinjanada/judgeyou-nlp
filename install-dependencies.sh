707d8406dd8e39f25e6ef9a7b92bb99bcbe7ad26
Kevin Janada <kevin.janada@gmail.com>
2019-05-25 04:39:58 +0700

    made changes to instal-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+# ----- AUDIO EXTRACTOR -------
+# Dependencies for download video from youtube and extract audio
 sudo apt-get install ffmpeg
 pip install opencv-python youtube-dl
+
+# ------ DEEPSPEECH -------------
+# Git LFS needed for deepspeech
+sudo add-apt-repository ppa:git-core/ppa
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
+# Get Dependencies
+sudo apt install libsox3 libstdc++6 libgomp1 libpthread-stubs0-dev sox
+# Get Deepspeech 
+pip install deepspeech
+# get pretrained model
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.4.1/deepspeech-0.4.1-models.tar.gz 
+tar xvfz deepspeech-0.4.1-models.tar.gz
